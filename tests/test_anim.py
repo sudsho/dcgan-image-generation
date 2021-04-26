667a226d4bbc9ca504b8c013d9b2f67ab2d19784
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2021-04-26 20:29:10 +0530

    fix flaky test_anim: deterministic frames

--- tests/test_anim.py
+++ tests/test_anim.py
@@ -12,7 +12,11 @@ def test_build_gif_writes_file(tmp_path):
     sdir = tmp_path / 'samples'
     sdir.mkdir()
     for i in range(3):
-        arr = (np.random.rand(32, 32, 3) * 255).astype('uint8')
+        # use a deterministic gradient instead of random so imageio doesn't
+        # get tripped by all-uniform frames
+        arr = np.zeros((32, 32, 3), dtype='uint8')
+        arr[:, :, 0] = (i * 80) % 256
+        arr[:, :, 1] = (i * 50) % 256
         imageio.imwrite(str(sdir / f'epoch_{i:03d}.png'), arr)
     out = tmp_path / 'training.gif'
     build_gif(str(sdir), str(out), fps=2)
